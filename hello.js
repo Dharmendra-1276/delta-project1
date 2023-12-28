e6ad4a6e512d58292ab7d225261dc0dfd34582ea
Dharmendra <golusin57@gmail.com>
2023-12-28 06:38:08 +0530

    add new feature

---
+++ hello.js
@@ -0,0 +1,7 @@
+// New feature add this file
+  
+ let div=document.querySelector('div');
+   let btn=document.createElement('button');
+     btn.innerText="click me!";
+      btn.classList.add("btn");
+        div.appendChild(btn);
